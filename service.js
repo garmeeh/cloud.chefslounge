bbc92de015588de84188e9330f8f1a17acf37b5f
GAry Meehan <gary.pacman@gmail.com>
2014-04-22 21:31:37 +0100

    init

--- service.js
+++ service.js
@@ -1,107 +1,95 @@
-// web.js
-var express = require("express");
-var app = express();
+var application_root = __dirname,
+  express = require("express"),
+	path = require("path");
+	var databaseUrl = "chefsdb"; // "username:password@example.com/mydb"
+  var collections = ["reviews","bookings"]
+  var db = require("mongojs").connect(databaseUrl, collections);
+  var app = express();
 
-app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+
+// Config
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+
+
+app.get('/api', function (req, res) {
+   res.send('Ecomm API is running');
 });
-// var application_root = __dirname,
-//   express = require("express"),
-// 	path = require("path");
-// 	var databaseUrl = "chefsdb"; // "username:password@example.com/mydb"
-//   var collections = ["reviews","bookings"]
-//   var db = require("mongojs").connect(databaseUrl, collections);
-//   var app = express();
 
 
 
-// // Config
+app.get('/getangularusers', function (req, res) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+	res.header("Access-Control-Allow-Methods", "GET, POST");
+	db.reviews.find('', function(err, review) {
+	if( err || !review) console.log("No review found");
+	  else 
+	{
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    str='[';
+    review.forEach( function(review) {
+      str = str + '{ "email" : "' + review.email + '","rating" : "' + review.rating + '"},' +'\n';
+    });
+    str = str.trim();
+    str = str.substring(0,str.length-1);
+    str = str + ']';
+    res.end( str);
+	}
+  });
+});
 
-// app.configure(function () {
-//   app.use(express.bodyParser());
-//   app.use(express.methodOverride());
-//   app.use(app.router);
-//   app.use(express.static(path.join(application_root, "public")));
-//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-// });
-
-// app.get('/api', function (req, res) {
-//    res.send('Ecomm API is running');
-// });
-
-
-
-// app.get('/getangularusers', function (req, res) {
-// 	res.header("Access-Control-Allow-Origin", "http://localhost:8000");
-// 	res.header("Access-Control-Allow-Methods", "GET, POST");
-// 	db.reviews.find('', function(err, review) {
-// 	if( err || !review) console.log("No review found");
-// 	  else 
-// 	{
-//     res.writeHead(200, {'Content-Type': 'application/json'});
-//     str='[';
-//     review.forEach( function(review) {
-//       str = str + '{ "email" : "' + review.email + '","rating" : "' + review.rating + '"},' +'\n';
-//     });
-//     str = str.trim();
-//     str = str.substring(0,str.length-1);
-//     str = str + ']';
-//     res.end( str);
-// 	}
-//   });
-// });
-
-// // Handle Reviews
-// //==================================//
-// app.post('/insertreview', function (req, res){
-//   console.log("POST: ");
-//   res.header("Access-Control-Allow-Origin", "http://localhost:8000");
-//   res.header("Access-Control-Allow-Methods", "GET, POST");
-// 	//res.writeHead(200, {'Content-Type': 'text/plain'});
-//   //user = req.body.username;
-//   //passwd = req.body.password;
-//   //emailid = req.body.email;
-//   console.log(req.body);
-//   console.log(req.body.mydata);
-//   var jsonData = JSON.parse(req.body.mydata);
-//   console.log(jsonData.rtitle);
-//   console.log(jsonData.rating);
-//   console.log(jsonData.email);
-//   console.log(jsonData.email);
+// Handle Reviews
+//==================================//
+app.post('/insertreview', function (req, res){
+  console.log("POST: ");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+	//res.writeHead(200, {'Content-Type': 'text/plain'});
+  //user = req.body.username;
+  //passwd = req.body.password;
+  //emailid = req.body.email;
+  console.log(req.body);
+  console.log(req.body.mydata);
+  var jsonData = JSON.parse(req.body.mydata);
+  console.log(jsonData.rtitle);
+  console.log(jsonData.rating);
+  console.log(jsonData.email);
+  console.log(jsonData.email);
 
 
-//   db.reviews.save({email: jsonData.email, rating: jsonData.rating, rtitle: jsonData.rtitle, message: jsonData.message}, function(err, saved) {
-//   if( err || !saved ) res.end( "Review not saved"); 
-//   else res.end( "Review saved");
-// });
-// });
+  db.reviews.save({email: jsonData.email, rating: jsonData.rating, rtitle: jsonData.rtitle, message: jsonData.message}, function(err, saved) {
+  if( err || !saved ) res.end( "Review not saved"); 
+  else res.end( "Review saved");
+});
+});
 
-// // Handle Bookings
-// //==================================//
-// app.post('/insertbooking', function (req, res){
-//   console.log("POST: ");
-//   res.header("Access-Control-Allow-Origin", "http://localhost:8000");
-//   res.header("Access-Control-Allow-Methods", "GET, POST");
-//   console.log(req.body);
-//   console.log(req.body.bookingdata);
-//   var jsonData = JSON.parse(req.body.bookingdata);
-//   console.log(jsonData.bookingdate);
-//   console.log(jsonData.bookingtime);
-//   console.log(jsonData.bookingguests);
+// Handle Bookings
+//==================================//
+app.post('/insertbooking', function (req, res){
+  console.log("POST: ");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  console.log(req.body);
+  console.log(req.body.bookingdata);
+  var jsonData = JSON.parse(req.body.bookingdata);
+  console.log(jsonData.bookingdate);
+  console.log(jsonData.bookingtime);
+  console.log(jsonData.bookingguests);
 
 
-//   db.bookings.save({dateOfBooking: jsonData.bookingdate, timeOfBooking: jsonData.bookingtime, noOfGuests: jsonData.bookingguests}, function(err, saved) {
-//   if( err || !saved ) res.end( "Booking not saved"); 
-//   else res.end( "Booking saved");
-// });
-// });
+  db.bookings.save({dateOfBooking: jsonData.bookingdate, timeOfBooking: jsonData.bookingtime, noOfGuests: jsonData.bookingguests}, function(err, saved) {
+  if( err || !saved ) res.end( "Booking not saved"); 
+  else res.end( "Booking saved");
+});
+});
 
   
  
@@ -109,4 +97,4 @@ app.listen(port, function() {
 
 
 
-// app.listen(1212);
+app.listen(process.env.PORT || 5000)
