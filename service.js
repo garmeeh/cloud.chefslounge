8de92400d14d434b93effd7b81b115eba4d523dc
GAry Meehan <gary.pacman@gmail.com>
2014-05-01 19:33:21 +0100

    Added offer

--- service.js
+++ service.js
@@ -125,7 +125,7 @@ app.post('/login', function(req, res) {
     console.log("POST: ");
 
     // var loginDetails = req.sbody.userdata;
-    console.log("aaa", JSON.parse(req));
+    console.log("aaa", req.body);
 
 
     db.admin.find({
